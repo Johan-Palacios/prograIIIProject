5290577bbafcb0632de9178c0eb92016e38163cc
Johan Palacios <johanpalacios.rivera@gmail.com>
2024-06-02 16:10:05 -0600

    feat: add forms

--- main.js
+++ main.js
@@ -13,6 +13,17 @@ document.querySelector('#app').innerHTML = `
     <main id="main_sections">
       <section id="pacientes_section">
         <h2>Pacientes</h2>
+        <div class="card">
+          <form id="paciente_add_form">
+            <label for="nombre_paciente_input">Nombre Paciente:</label>
+            <input type="text" name="nombre" value="" placeholder="Ej. Paco Perez" id="nombre_paciente_input" autocomplete="off" required>
+            <label for="edad_paciente_input">Edad Paciente:</label>
+            <input type="number" name="Edad" min="1" max="100" placeholder="Ej. 30" id="edad_paciente_input" autocomplete="off" required>
+            <label for="genero_paciente_input">Genero Paciente:</label>
+            <input type="text" name="Genero" value="" placeholder="Ej. Masculino" id="genero_paciente_input" autocomplete="off" required>
+            <button type="submit" id="add_paciente_button">Ingresar Paciente</button>
+          </form>
+        </div>
         <table>
           <thead>
             <tr>
@@ -29,6 +40,15 @@ document.querySelector('#app').innerHTML = `
       </section>
       <section id="doctores_section">
         <h2>Doctores</h2>
+        <div class="card">
+          <form id="doctor_add_form">
+            <label for="nombre_doctor_input">Nombre doctor:</label>
+            <input type="text" name="nombre" value="" placeholder="Ej. Menso Ruiz" id="nombre_doctor_input" autocomplete="off" required>
+            <label for="especialidad_doctor_input">Especialidad doctor:</label>
+            <input type="text" name="Especialidad" placeholder="Ej. Cardiologo" id="especialidad_doctor_input" autocomplete="off" required>
+            <button type="submit" id="add_doctor_button">Ingresar doctor</button>
+          </form>
+        </div>
         <table>
           <thead>
             <tr>
@@ -44,6 +64,17 @@ document.querySelector('#app').innerHTML = `
       </section>
       <section id="citas_section">
         <h2>Citas</h2>
+        <div class="card">
+          <form id="cita_add_form">
+            <label for="paciente_cita_input">Id Paciente:</label>
+            <input type="number" name="id paciente" value="" placeholder="Ej. 1" id="paciente_cita_input" autocomplete="off" required>
+            <label for="doctor_cita_input">Doctor cita:</label>
+            <input type="text" name="Especialidad" placeholder="Ej. 2" id="doctor_cita_input" autocomplete="off" required>
+            <label for="date_cita_input">Doctor cita:</label>
+            <input type="date" name="Fecha" placeholder="Ingrese Fecha de Cita" id="date_cita_input" autocomplete="off" required>
+            <button type="submit" id="add_cita_button">Ingresar cita</button>
+          </form>
+        </div>
         <table>
           <thead>
             <tr>
